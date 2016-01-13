65b0c83ea7423b3638578c10e3351221266b7f92
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-01-13 09:56:16 +0700

    tag: source

---
+++ modules/album/views/o/tag/admin_edit.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Album Tags (album-tag)
+ * @var $this TagController
+ * @var $model AlbumTag
+ * @var $form CActiveForm
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2015 Ommu Platform (ommu.co)
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Album Tags'=>array('manage'),
+		$model->id=>array('view','id'=>$model->id),
+		'Update',
+	);
+?>
+
+<div class="form">
+	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
---
+++ modules/album/views/o/tag/front_view.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Album Tags (album-tag)
+ * @var $this TagController
+ * @var $model AlbumTag
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2015 Ommu Platform (ommu.co)
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Album Tags'=>array('manage'),
+		$model->id,
+	);
+?>
+
+<?php //begin.Messages ?>
+<?php
+if(Yii::app()->user->hasFlash('success'))
+	echo Utility::flashSuccess(Yii::app()->user->getFlash('success'));
+?>
+<?php //end.Messages ?>
+
+<?php $this->widget('application.components.system.FDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		'id',
+		'album_id',
+		'tag_id',
+		'creation_date',
+		'creation_id',
+	),
+)); ?>
+
+<div class="dialog-content">
+</div>
+<div class="dialog-submit">
+	<?php echo CHtml::button(Phrase::trans(4,0), array('id'=>'closed')); ?>
+</div>
