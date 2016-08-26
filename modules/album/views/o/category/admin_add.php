917a2e2d894329f78fa062da2434cb291dcd19b3
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-08-27 00:23:04 +0700

    category: fixed based from bpadjogja.info

---
+++ modules/album/views/o/category/admin_add.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Album Categories (album-category)
+ * @var $this CategoryController
+ * @var $model AlbumCategory
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 26 August 2016, 23:10 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Album Categories'=>array('manage'),
+		'Create',
+	);
+?>
+
+<div class="form" name="post-on">
+	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
