9c45c3c8ec571233925c5b772472456ca5e61423
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-06-05 15:01:03 +0700

    manual: commit

--- modules/album/models/AlbumCategory.php
+++ modules/album/models/AlbumCategory.php
@@ -490,8 +490,8 @@ class AlbumCategory extends CActiveRecord
 	 */
 	protected function beforeSave() 
 	{
-		$currentAction = strtolower(Yii::app()->controller->id.'/'.Yii::app()->controller->action->id);
-		$location = Utility::getUrlTitle($currentAction);
+		$currentModule = strtolower(Yii::app()->controller->module->id.'/'.Yii::app()->controller->id);
+		$location = Utility::getUrlTitle($currentModule);
 		
 		if(parent::beforeSave()) {
 			if($this->isNewRecord || (!$this->isNewRecord && $this->name == 0)) {
