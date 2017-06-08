d7e775210b8210b1d6bcbff9aa74b7c5c8d0d317
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-06-08 23:45:13 +0700

    modify manual menu

--- modules/album/controllers/o/SettingController.php
+++ modules/album/controllers/o/SettingController.php
@@ -174,7 +174,7 @@ class SettingController extends Controller
 		$this->dialogGroundUrl = Yii::app()->controller->createUrl('o/admin/manage');
 		$this->dialogWidth = 400;
 		
-		$this->pageTitle = Yii::t('phrase', 'Downlaod Manual Book');
+		$this->pageTitle = Yii::t('phrase', 'Album Photo Manual Book');
 		$this->pageDescription = '';
 		$this->pageMeta = '';
 		$this->render('admin_manual', array(
