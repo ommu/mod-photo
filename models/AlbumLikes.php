b1a25b5a6eec7a179f699eca51f73e0e5bcf2546
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-04-03 13:28:34 +0700

    commit

--- models/AlbumLikes.php
+++ models/AlbumLikes.php
@@ -351,7 +351,7 @@ class AlbumLikes extends CActiveRecord
 	protected function beforeValidate() {
 		if(parent::beforeValidate()) {		
 			if($this->isNewRecord)
-				$this->user_id = !Yii::app()->user->isGuest ? Yii::app()->user->id : 0;
+				$this->user_id = !Yii::app()->user->isGuest ? Yii::app()->user->id : null;
 				
 			$this->likes_ip = $_SERVER['REMOTE_ADDR'];
 		}
