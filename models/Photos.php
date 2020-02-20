90e7c285acb2320d628798fa428d61a511dcb664
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2020-02-20 10:34:59 +0700

    change SerialColumn use Class

--- models/Photos.php
+++ models/Photos.php
@@ -172,7 +172,7 @@ class Photos extends \app\components\ActiveRecord
 
 		$this->templateColumns['_no'] = [
 			'header' => '#',
-			'class' => 'yii\grid\SerialColumn',
+			'class' => 'app\components\grid\SerialColumn',
 			'contentOptions' => ['class'=>'text-center'],
 		];
 		$this->templateColumns['memberDisplayname'] = [
